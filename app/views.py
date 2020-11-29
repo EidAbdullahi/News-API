310a1eaf6e3be3f5ce4ccdb42cb56cf34df5e1c0
EidAbdullahi <eidabdullahi10@gmail.com>
2020-11-29 07:59:29 +0300

    commit all changes

--- app/views.py
+++ app/views.py
@@ -1,11 +1,29 @@
-from flask import render_template
+from flask import render_template,request,redirect,url_for
 from app import app
+from .request import get_news,get_article
 
-# Views
-@app.route('/')
+
+#views
+@app.route('/', methods=['GET'])
 def index():
 
     '''
-    View root page function that returns the index page and its data
+    view news page function that returns the news details  and its data
     '''
-    return render_template('index.html')
+    
+    news = get_news()
+    # print(news)
+    title = "News_Highlight"
+    
+    return render_template('news.html', news = news, title=title)
+
+@app.route('/source/<id>')
+def article(id):
+    '''
+    view news page function that returns the news details  and its data
+    '''
+    
+    articles = get_article(id)
+    title = "News_Highlight"
+    
+    return render_template('article.html', articles = articles, title=title)
